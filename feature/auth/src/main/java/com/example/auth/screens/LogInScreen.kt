02023829e3d170ff837656d38276f8bfe80e2866
Jamik <dzamikhidirov8@gmail.com>
2026-04-29 17:59:50 +0300

    Добавляем экран логина и исправлен гитигнор

---
+++ feature/auth/src/main/java/com/example/auth/screens/LogInScreen.kt
@@ -0,0 +1,24 @@
+package com.example.auth.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Preview(showBackground = true)
+@Composable
+fun LogInScreen(){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
+
---
+++ feature/auth/src/main/java/com/example/auth/uicomponents/CardLogInScreen.kt
@@ -0,0 +1,17 @@
+package com.example.auth.uicomponents
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+@Preview(showBackground = true)
+fun CardLogInScreen(
+    modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier
+    ) { }
+}
