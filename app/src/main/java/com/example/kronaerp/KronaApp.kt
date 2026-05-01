88b4ae4ba7dbe007fe5c76fd660046676f0d2895
Jamik <dzamikhidirov8@gmail.com>
2026-05-01 18:52:50 +0300

    Добавляем логику и экран мои заказы

--- app/src/main/java/com/example/kronaerp/KronaApp.kt
+++ app/src/main/java/com/example/kronaerp/KronaApp.kt
@@ -1,9 +1,16 @@
 package com.example.kronaerp
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
 class KronaApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Явная инициализация Firebase ДО того, как Hilt начнёт инжектить зависимости
+        FirebaseApp.initializeApp(this)
+    }
 }
