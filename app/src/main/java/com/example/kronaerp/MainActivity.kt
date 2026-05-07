f31bff2a10c953504a9949cec9d79327553ea7bf
Jamik <dzamikhidirov8@gmail.com>
2026-05-07 16:06:15 +0300

    Добавлен сплеш скрин, добавлена иконка приложения

--- app/src/main/java/com/example/kronaerp/MainActivity.kt
+++ app/src/main/java/com/example/kronaerp/MainActivity.kt
@@ -5,20 +5,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.example.auth.screens.login.LogInScreen
 import com.example.auth.screens.register.RegisterScreen
 import com.example.kronaerp.ui.theme.KronaERPTheme
 import com.example.navigation.KronaAppNavigation
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private var keepSplashScreenOn = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         val splashScreen = installSplashScreen()
+
+        splashScreen.setKeepOnScreenCondition { keepSplashScreenOn }
+
+
+        lifecycleScope.launch {
+            delay(1500)  // Задержка 1.5 секунды
+            keepSplashScreenOn = false
+        }
+
 
         setContent {
             KronaERPTheme {
